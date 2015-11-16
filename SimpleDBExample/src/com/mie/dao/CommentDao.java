f6547eb76313782cb0aa999c62d46fc04736a328
marissazhang <marissazhng@gmail.com>
2015-11-16 15:56:00 -0500

    added commenting feature

--- SimpleDBExample/src/com/mie/dao/CommentDao.java
+++ SimpleDBExample/src/com/mie/dao/CommentDao.java
@@ -7,8 +7,10 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
 import com.mie.model.Thread;
 import com.mie.model.Comment;
+import com.mie.model.User;
 import com.mie.util.DbUtil;
 
 public class CommentDao {
@@ -19,7 +21,21 @@ public class CommentDao {
 		connection = DbUtil.getConnection();
 	}
 
+	public void addComment(Comment comment) {
+		try {
+			PreparedStatement preparedStatement = connection
+					.prepareStatement("insert into Comments(UserID,ThreadID,Comment) values (?, ?, ?)");
+			// Parameters start with 1
+			preparedStatement.setInt(1, comment.getUserID());
+			preparedStatement.setInt(2, comment.getThreadID());
+			preparedStatement.setString(3, comment.getComment());
+			preparedStatement.executeUpdate();
 
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 
 	public List<Comment> getAllComments(int id) {
 		List<Comment> comments = new ArrayList<Comment>();
@@ -30,6 +46,7 @@ public class CommentDao {
 				Comment comment = new Comment();
 				comment.setCommentID(rs.getInt("CommentID"));
 				comment.setUserID(rs.getInt("UserID"));
+				comment.setThreadID(rs.getInt("ThreadID"));
 				comment.setComment(rs.getString("Comment"));
 				comment.setTimestamp(rs.getDate("Timestamp"));
 				comments.add(comment);
